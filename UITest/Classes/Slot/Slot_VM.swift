352a8d836df2c277ce1035a34b611e0debe9be04
Raymond <raymond@spoonradio.co>
2021-03-10 09:29:50 +0900

    Slot VC 추가

---
+++ UITest/Classes/Slot/Slot_VM.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+
+class Slot_VM {
+
+    private let disposeBag = DisposeBag()
+
+
+    init() {
+        self.load()
+    }
+}
+
+
+extension Slot_VM {
+
+    private func load() {
+    }
+}
