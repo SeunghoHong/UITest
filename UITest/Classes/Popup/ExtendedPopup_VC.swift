5555d7dcd43b4af51531b179250c6c04e661c7ff
raymond <raymond@spoonradio.co>
2021-06-30 15:29:25 +0900

    extend popup에 tableView 연동

--- UITest/Classes/Popup/ExtendedPopup_VC.swift
+++ UITest/Classes/Popup/ExtendedPopup_VC.swift
@@ -12,17 +12,31 @@ internal class EntendedPopup_VC: UIViewController {
     private let dimmedView = UIView()
     private let contentView = UIView()
 
+    private let infoView = UIView()
+    private let titleLabel = UILabel()
+
+    private let tableView = UITableView()
+
     private var contentViewTopConstraint: Constraint?
 
-    private var maxHeight: CGFloat = UIScreen.main.bounds.height
+    private var maxHeight: CGFloat = UIScreen.main.bounds.height - 64.0
     private var defaultHeight: CGFloat = 300.0
     private var currentHeight: CGFloat = 300.0
+    private var locationY: CGFloat = 0.0
+
+    private var newHeight = PublishRelay<CGFloat>()
 
     private var disposeBag = DisposeBag()
 
+    private var data: [Int] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        (1..<30).forEach {
+            self.data.append($0)
+        }
 
         self.setup()
         self.layout()
@@ -52,6 +66,19 @@ extension EntendedPopup_VC {
 
         self.contentView.backgroundColor = .yellow
         self.view.addSubview(self.contentView)
+
+        self.infoView.backgroundColor = .lightGray
+        self.contentView.addSubview(self.infoView)
+
+        self.titleLabel.text = "Extendable Popup"
+        self.infoView.addSubview(self.titleLabel)
+
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        self.tableView.height = 44.0
+        self.tableView.bounces = false
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.contentView.addSubview(self.tableView)
     }
 
     private func layout() {
@@ -64,6 +91,20 @@ extension EntendedPopup_VC {
             maker.height.equalToSuperview()
             self.contentViewTopConstraint = maker.top.equalTo(self.view.snp.bottom).constraint
         }
+
+        self.infoView.snp.makeConstraints { maker in
+            maker.top.leading.trailing.equalToSuperview()
+            maker.height.equalTo(64.0)
+        }
+
+        self.titleLabel.snp.makeConstraints { maker in
+            maker.center.equalToSuperview()
+        }
+
+        self.tableView.snp.makeConstraints { maker in
+            maker.top.equalTo(self.infoView.snp.bottom)
+            maker.bottom.leading.trailing.equalToSuperview()
+        }
     }
 
     private func bind() {
@@ -74,6 +115,14 @@ extension EntendedPopup_VC {
             }
             .disposed(by: self.disposeBag)
 
+        self.newHeight.asDriver(onErrorJustReturn: self.defaultHeight)
+            .distinctUntilChanged()
+            .drive() {
+                self.contentViewTopConstraint?.update(offset: -$0)
+                self.view.layoutIfNeeded()
+            }
+            .disposed(by: self.disposeBag)
+
         self.contentView.rx.panGesture()
             .bind { [weak self] gesture in
                 guard let self = self else { return }
@@ -81,25 +130,29 @@ extension EntendedPopup_VC {
                 let velocity = gesture.velocity(in: self.view)
                 let isDraggingDown = velocity.y > 0
 
-                let translation = gesture.translation(in: self.view)
-                let newHeight = self.currentHeight - translation.y
+                let location = gesture.location(in: self.view)
+
+                defer {
+                    self.locationY = location.y
+                }
 
                 switch gesture.state {
                 case .changed:
-                    if newHeight < self.maxHeight {
-                        self.contentViewTopConstraint?.update(offset: -newHeight)
-                        self.view.layoutIfNeeded()
-                    }
+                    guard self.tableView.contentOffset.y == 0.0 else { return }
+
+                    self.currentHeight += (self.locationY - location.y)
+                    self.currentHeight = min(self.maxHeight, self.currentHeight)
+                    self.newHeight.accept(self.currentHeight)
                 case .ended:
-                    if newHeight < self.defaultHeight {
+                    if self.currentHeight < self.defaultHeight {
                         if isDraggingDown {
                             self.dismissAnimation()
                         } else {
                             self.animateHeight(self.defaultHeight)
                         }
-                    } else if newHeight < self.maxHeight && isDraggingDown {
+                    } else if self.currentHeight < self.maxHeight && isDraggingDown {
                         self.animateHeight(self.defaultHeight)
-                    } else if newHeight > self.defaultHeight && !isDraggingDown {
+                    } else if self.currentHeight > self.defaultHeight && !isDraggingDown {
                         self.animateHeight(self.maxHeight)
                     }
                 default:
@@ -151,5 +204,26 @@ extension EntendedPopup_VC {
         }
 
         self.currentHeight = height
+    }
+}
+
+
+extension EntendedPopup_VC: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = "\(self.data[indexPath.row])"
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if let constant = self.contentViewTopConstraint?.layoutConstraints.first?.constant, -constant < self.maxHeight {
+            self.tableView.contentOffset = .zero
+        }
     }
 }
