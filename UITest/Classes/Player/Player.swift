97b8ed87385a47b86bc9a3311a6e8012ab9d268d
raymond <raymond@spoonradio.co>
2021-09-07 20:13:07 +0900

    Player Test UI

---
+++ UITest/Classes/Player/Player.swift
@@ -0,0 +1,10 @@
+
+import AVFoundation
+
+import RxSwift
+import RxCocoa
+
+
+extension Reactive where Base: AVPlayer {
+    
+}
--- UITest/Classes/Player/Player_VC.swift
+++ UITest/Classes/Player/Player_VC.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import AVFoundation
 
 import RxSwift
 import RxCocoa
@@ -7,60 +8,17 @@ import PinLayout
 import SnapKit
 
 
-final class CircleProgressBar: UIView {
-
-    private var progressLayer = CAShapeLayer()
-    private var backgroundMask = CAShapeLayer()
-
-    var progress: CGFloat = 0.0 {
-        didSet { self.setNeedsDisplay() }
-    }
-
-
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    init() {
-        super.init(frame: .zero)
-
-        self.setup()
-    }
-    
-    private func setup() {
-        self.backgroundMask.lineWidth = 4.0
-        self.backgroundMask.fillColor = nil
-        self.backgroundMask.strokeColor = UIColor.black.cgColor
-        self.layer.mask = self.backgroundMask
-
-        self.progressLayer.lineWidth = 4.0
-        self.progressLayer.fillColor = nil
-        self.layer.addSublayer(self.progressLayer)
-        self.layer.transform = CATransform3DMakeRotation(CGFloat(90.0 * .pi / 180.0), 0, 0, -1)
-    }
-
-    override func draw(_ rect: CGRect) {
-        let circlePath = UIBezierPath(ovalIn: rect.insetBy(dx: 4.0 / 2, dy: 4.0 / 2))
-        self.backgroundMask.path = circlePath.cgPath
-
-        self.progressLayer.path = circlePath.cgPath
-        self.progressLayer.lineCap = .round
-        self.progressLayer.strokeStart = 0.0
-        self.progressLayer.strokeEnd = self.progress
-        self.progressLayer.strokeColor = UIColor.red.cgColor
-        self.progressLayer.backgroundColor = UIColor.black.cgColor
-    }
-}
-
-
-
 final class Player_VC: UIViewController {
 
     private let closeButton = UIButton()
 
-    private let backgroundImageView = UIImageView()
-    private let circleProgressBar = CircleProgressBar()
-    private let profileImageView = UIImageView()
-
-    private let titleLabel = UILabel()
-    private let nicknameLabel = UILabel()
+    private lazy var pageViewController: UIPageViewController = {
+        return UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal,
+            options: nil
+        )
+    }()
 
     private var disposeBag = DisposeBag()
 
@@ -93,23 +51,17 @@ extension Player_VC {
     private func setup() {
         self.view.backgroundColor = .white
 
-        self.backgroundImageView.backgroundColor = .lightGray
-        self.view.addSubview(self.backgroundImageView)
+//        self.pageViewController.delegate = self
+        self.pageViewController.dataSource = self
+        self.view.addSubview(self.pageViewController.view)
+        self.addChild(self.pageViewController)
 
-        self.circleProgressBar.backgroundColor = .black
-        self.circleProgressBar.progress = 0.5
-        self.view.addSubview(self.circleProgressBar)
-
-        self.profileImageView.clipsToBounds = true
-        self.profileImageView.backgroundColor = .blue
-        self.profileImageView.radius = 100.0
-        self.view.addSubview(self.profileImageView)
-
-        self.titleLabel.backgroundColor = .green
-        self.view.addSubview(self.titleLabel)
-
-        self.nicknameLabel.backgroundColor = .yellow
-        self.view.addSubview(self.nicknameLabel)
+        self.pageViewController.setViewControllers(
+            [PlayerDetail_VC()],
+            direction: .forward,
+            animated: true,
+            completion: nil
+        )
 
         self.closeButton.backgroundColor = .black
         self.closeButton.setTitle("close", for: .normal)
@@ -123,24 +75,34 @@ extension Player_VC {
             .marginTop(16.0)
             .sizeToFit()
 
-        self.circleProgressBar.pin
-            .below(of: self.closeButton)
-            .margin(10.0)
-            .hCenter()
-            .size(212.0)
-
-        self.profileImageView.pin
-            .below(of: self.closeButton)
-            .marginTop(16.0)
-            .hCenter()
-//            .width(30%)
-            .size(200.0)
-
-        self.backgroundImageView.pin
+        self.pageViewController.view.pin
             .all()
     }
 
     private func bind() {
+        self.closeButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .bind { [weak self] _ in
+                self?.dismiss(animated: true, completion: nil)
+            }
+            .disposed(by: self.disposeBag)
+    }
+}
 
+
+extension Player_VC: UIPageViewControllerDataSource {
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+        return PlayerDetail_VC()
+    }
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+        return PlayerDetail_VC()
     }
 }
